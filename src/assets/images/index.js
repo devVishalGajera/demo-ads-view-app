7176db97be8ce57885391a289a6682ad1254f2da
Vishal Gajera <vishalgajera101@gmail.com>
2023-03-21 17:53:29 +0530

    added images

---
+++ src/assets/images/index.js
@@ -0,0 +1,21 @@
+import action1 from './action-1.png';
+import action2 from './action-2.jpg';
+import action3 from './action-3.jpg';
+import action4 from './action-4.jpg';
+import action5 from './action-5.jpg';
+import action6 from './action-6.png';
+import action7 from './action-7.jpg';
+import action8 from './action-8.jpg';
+import action9 from './action-9.jpg';
+
+export {
+    action1,
+    action2,
+    action3,
+    action4,
+    action5,
+    action6,
+    action7,
+    action8,
+    action9,
+}
--- src/components/GameListPage.js
+++ src/components/GameListPage.js
@@ -1,6 +1,63 @@
 import React, {useEffect, useState} from "react";
 import {Row, Col} from "react-bootstrap";
 import FullScreenAd from "./FullScreenView";
+import {action1, action2, action3, action4, action5, action6, action7, action8, action9} from '../assets/images'
+
+const gamesList = [{
+    id: 1,
+    mainBanner: action1,
+    logoBanner: action1,
+    appName: '',
+    companyName: ''
+}, {
+    id: 2,
+    mainBanner: action2,
+    logoBanner: action2,
+    appName: '',
+    companyName: ''
+}, {
+    id: 3,
+    mainBanner: action3,
+    logoBanner: action3,
+    appName: '',
+    companyName: ''
+}, {
+    id: 4,
+    mainBanner: action4,
+    logoBanner: action4,
+    appName: '',
+    companyName: ''
+}, {
+    id: 5,
+    mainBanner: action5,
+    logoBanner: action5,
+    appName: '',
+    companyName: ''
+}, {
+    id: 6,
+    mainBanner: action6,
+    logoBanner: action6,
+    appName: '',
+    companyName: ''
+}, {
+    id: 7,
+    mainBanner: action7,
+    logoBanner: action7,
+    appName: '',
+    companyName: ''
+}, {
+    id: 8,
+    mainBanner: action8,
+    logoBanner: action8,
+    appName: '',
+    companyName: ''
+}, {
+    id: 9,
+    mainBanner: action9,
+    logoBanner: action9,
+    appName: '',
+    companyName: ''
+},];
 
 const GameListPage = (props) => {
     const {isFrom} = props;
@@ -23,30 +80,54 @@ const GameListPage = (props) => {
                 <iframe src="http://1117.set.qureka.com" title="Quiz" width="100%" height="100%"/>
             </div>
             <Row>
-                {[...Array(30)].map((_, i) => {
+                {[...gamesList].map((item, i) => {
                     if (divide / i === 0 || divide / i === 1) {
                         divide = divide + 7;
-                        return (<Col md={12} lg={12} xxl={12} sm={12}>
-                            <div className='add-card h-200'>
-                                <iframe src="http://1117.set.qureka.com" title="Quiz" width="100%" height="100%"/>
-                            </div>
-                        </Col>)
+                        return (<><Col md={12} lg={12} xxl={12} sm={12}>
+                                <div className='add-card h-200'>
+                                    <iframe src="http://1117.set.qureka.com" title="Quiz" width="100%" height="100%"/>
+                                </div>
+                            </Col>
+                                <Col md={4} lg={4} xxl={4} sm={4}
+                                     className="d-flex flex-column game-list-card p-3"
+                                     onClick={() => setShowAd(!showAd)}>
+                                    <img
+                                        className='rounded shadow h-100 w-100'
+                                        style={{maxHeight: '220px', maxWidth: '350px'}}
+                                        src={item?.mainBanner}
+                                        alt=''/>
+                                    <div className='mt-3 d-flex gap-3'>
+                                        <img
+                                            className='rounded shadow'
+                                            src={item?.logoBanner}
+                                            style={{height: '90px', width: '90px', objectFit: 'cover'}}
+                                            alt=''/>
+                                        <div>
+                                            <p className='m-0'><strong>{item?.appName}</strong></p>
+                                            <p className='m-0'><small>{item?.companyName}</small></p>
+                                        </div>
+                                    </div>
+                                </Col>
+                            </>
+                        )
                     } else {
                         return (<Col md={4} lg={4} xxl={4} sm={4}
                                      className="d-flex flex-column game-list-card p-3"
                                      onClick={() => setShowAd(!showAd)}>
                             <img
-                                className='rounded shadow'
-                                src='https://play-lh.googleusercontent.com/_Ub7v4j6_rVA26PX7KkAwCLaF6nfsrpwMfh1mfhukwp6FLNrc6vHqwn1CwffBR7PUuc=w416-h235-rw'
+                                className='rounded shadow h-100 w-100'
+                                style={{maxHeight: '220px', maxWidth: '350px'}}
+                                src={item?.mainBanner}
                                 alt=''/>
                             <div className='mt-3 d-flex gap-3'>
                                 <img
                                     className='rounded shadow'
-                                    src='https://play-lh.googleusercontent.com/_dDFSU1PlfewLJHAeY8dd4xjUV5-7oBmCRZdLyIh-DV4q71NaeQsf8bNSv8S95lmCPs=s64-rw'
+                                    src={item?.logoBanner}
+                                    style={{height: '90px', width: '90px', objectFit: 'cover'}}
                                     alt=''/>
                                 <div>
-                                    <p className='m-0'><strong>Mobile Bank : Balance Checker</strong></p>
-                                    <p className='m-0'><small>Soul Celestia Solutions</small></p>
+                                    <p className='m-0'><strong>{item?.appName}</strong></p>
+                                    <p className='m-0'><small>{item?.companyName}</small></p>
                                 </div>
                             </div>
                         </Col>)
